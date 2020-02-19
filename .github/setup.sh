35d5460e9c0d4b6e2c3112a821735508fa94a247
Gregor Sturm <mail@gregor-sturm.de>
2020-02-19 16:10:01 +0100

    change bioconda-utils tag

--- .github/setup.sh
+++ .github/setup.sh
@@ -3,7 +3,7 @@ WORKSPACE=$(pwd)
 
 ## Common definitions for the bioconda build system. 
 MINICONDA_VER=4.6.14
-BIOCONDA_UTILS_TAG=v0.16.8
+BIOCONDA_UTILS_TAG=master
 
 if ! type bioconda-utils 2> /dev/null || [[ $BOOTSTRAP == "true" ]]; then
     echo "Setting up bioconda-utils..."
