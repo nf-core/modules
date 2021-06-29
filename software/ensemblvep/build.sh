93b5bae7b01b4bf5be4e7407d4a5c0e0ce3f8da3
MaxUlysse <max.u.garcia@gmail.com>
2021-06-29 13:23:39 +0200

    fix: improve script

--- software/ensemblvep/build.sh
+++ software/ensemblvep/build.sh
@@ -1,47 +1,27 @@
-export GENOME=GRCh37
-export SPECIES=homo_sapiens
-export VEP_VERSION=104
-export VEP_TAG=104.3
+#!/usr/bin/env bash
+set -euo pipefail
 
-docker build -t nfcore/vep:${VEP_TAG}.${GENOME} modules/nf-core/software/vep/. --build-arg GENOME=${GENOME} --build-arg SPECIES=${SPECIES} --build-arg VEP_VERSION=${VEP_VERSION}
-docker push nfcore/vep:${VEP_TAG}.${GENOME}
+# Build and push all containers
 
-export GENOME=GRCh38
-export SPECIES=homo_sapiens
-export VEP_VERSION=104
-export VEP_TAG=104.3
+build_push() {
+    GENOME=$1
+    SPECIES=$2
+    VEP_VERSION=$3
+    VEP_TAG=$4
 
-docker build -t nfcore/vep:${VEP_TAG}.${GENOME} modules/nf-core/software/vep/. --build-arg GENOME=${GENOME} --build-arg SPECIES=${SPECIES} --build-arg VEP_VERSION=${VEP_VERSION}
-docker push nfcore/vep:${VEP_TAG}.${GENOME}
+    docker build \
+        -t nfcore/vep:${VEP_TAG}.${GENOME} \
+        software/vep/. \
+        --build-arg GENOME=${GENOME} \
+        --build-arg SPECIES=${SPECIES} \
+        --build-arg VEP_VERSION=${VEP_VERSION}
 
-export GENOME=GRCm39
-export SPECIES=mus_musculus
-export VEP_VERSION=104
-export VEP_TAG=104.3
+    docker push nfcore/vep:${VEP_TAG}.${GENOME}
+}
 
-docker build -t nfcore/vep:${VEP_TAG}.${GENOME} modules/nf-core/software/vep/. --build-arg GENOME=${GENOME} --build-arg SPECIES=${SPECIES} --build-arg VEP_VERSION=${VEP_VERSION}
-docker push nfcore/vep:${VEP_TAG}.${GENOME}
-
-export GENOME=GRCm38
-export SPECIES=mus_musculus
-export VEP_VERSION=102
-export VEP_TAG=104.3
-
-docker build -t nfcore/vep:${VEP_TAG}.${GENOME} modules/nf-core/software/vep/. --build-arg GENOME=${GENOME} --build-arg SPECIES=${SPECIES} --build-arg VEP_VERSION=${VEP_VERSION}
-docker push nfcore/vep:${VEP_TAG}.${GENOME}
-
-export GENOME=CanFam3.1
-export SPECIES=canis_lupus_familiaris
-export VEP_VERSION=104
-export VEP_TAG=104.3
-
-docker build -t nfcore/vep:${VEP_TAG}.${GENOME} modules/nf-core/software/vep/. --build-arg GENOME=${GENOME} --build-arg SPECIES=${SPECIES} --build-arg VEP_VERSION=${VEP_VERSION}
-docker push nfcore/vep:${VEP_TAG}.${GENOME}
-
-export GENOME=WBcel235
-export SPECIES=caenorhabditis_elegans
-export VEP_VERSION=104
-export VEP_TAG=104.3
-
-docker build -t nfcore/vep:${VEP_TAG}.${GENOME} modules/nf-core/software/vep/. --build-arg GENOME=${GENOME} --build-arg SPECIES=${SPECIES} --build-arg VEP_VERSION=${VEP_VERSION}
-docker push nfcore/vep:${VEP_TAG}.${GENOME}
+build_push "GRCh37"    "homo_sapiens"           "104" "104.3"
+build_push "GRCh38"    "homo_sapiens"           "104" "104.3"
+build_push "GRCm38"    "mus_musculus"           "102" "104.3"
+build_push "GRCm39"    "mus_musculus"           "104" "104.3"
+build_push "CanFam3.1" "canis_lupus_familiaris" "104" "104.3"
+build_push "WBcel235"  "caenorhabditis_elegans" "104" "104.3"
