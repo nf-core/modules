f98ceeed86c7b0e815cb7fd74cf0cbffc8040557
Gregor Sturm <mail@gregor-sturm.de>
2021-08-16 17:23:20 +0200

    Add docstring

--- tests/test_versions_yml.py
+++ tests/test_versions_yml.py
@@ -4,6 +4,10 @@ import yaml
 
 
 def _get_workflow_names():
+    """Get all names of all workflows which have a test.yml in the tests directory.
+
+    To do so, recursively finds all test.yml files and parses their content.
+    """
     here = Path(__file__).parent.resolve()
     pytest_workflow_files = here.glob("**/test.yml")
     for f in pytest_workflow_files:
@@ -18,4 +22,5 @@ def test_ensure_valid_version_yml(workflow_dir):
     software_name = workflow_dir.name.split("_")[0].lower()
     versions_yml = (workflow_dir / f"output/{software_name}/versions.yml").read_text()
     assert "END_VERSIONS" not in versions_yml
+    # Raises an exception if yaml is not valid
     yaml.safe_load(versions_yml)
