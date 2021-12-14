975347a169375c02069b5abe3d6db071e03c8a14
Phil Ewels <phil.ewels@scilifelab.se>
2021-12-14 10:52:33 +0100

    Count how many modules the duplicates affect

--- .github/check_duplicate_md5s.py
+++ .github/check_duplicate_md5s.py
@@ -26,7 +26,8 @@ def find_duplicate_md5s(min_dups, search_dir):
     Find duplicate file MD5 sums in test YAML files.
     """
     md5_filenames = {}
-    md5_counts = {}
+    md5_output_fn_counts = {}
+    module_counts = {}
 
     # Loop through all files in tests/ called test.yml
     for test_yml in glob.glob(search_dir, recursive=True):
@@ -41,18 +42,38 @@ def find_duplicate_md5s(min_dups, search_dir):
                         md5_filenames[md5] = md5_filenames.get(md5, []) + [
                             os.path.basename(test_file.get("path"))
                         ]
-                        md5_counts[md5] = md5_counts.get(md5, 0) + 1
+                        md5_output_fn_counts[md5] = md5_output_fn_counts.get(md5, 0) + 1
+                        # Log the module that this md5 was in
+                        modname = os.path.basename(os.path.dirname(test_yml))
+                        # If tool/subtool show the whole thing
+                        # Ugly code but trying to stat os-agnostic
+                        if os.path.basename(
+                            os.path.dirname(os.path.dirname(test_yml))
+                        ) not in ["modules", "config", "subworkflows"]:
+                            modname = "{}/{}".format(
+                                os.path.basename(
+                                    os.path.dirname(os.path.dirname(test_yml))
+                                ),
+                                os.path.basename(os.path.dirname(test_yml)),
+                            )
+                        module_counts[md5] = module_counts.get(md5, []) + [modname]
 
     # Set up rich table
     table = Table(title="Duplicate MD5s", row_styles=["dim", ""])
     table.add_column("MD5", style="cyan", no_wrap=True)
     table.add_column("Count", style="magenta", justify="right")
+    table.add_column("Num modules", style="blue", justify="right")
     table.add_column("Filenames", style="green")
 
-    # Add rows - sort md5_counts by value
-    for md5 in sorted(md5_counts, key=md5_counts.get):
-        if md5_counts[md5] >= min_dups:
-            table.add_row(md5, str(md5_counts[md5]), ", ".join(set(md5_filenames[md5])))
+    # Add rows - sort md5_output_fn_counts by value
+    for md5 in sorted(md5_output_fn_counts, key=md5_output_fn_counts.get):
+        if md5_output_fn_counts[md5] >= min_dups:
+            table.add_row(
+                md5,
+                str(md5_output_fn_counts[md5]),
+                str(len(set(module_counts[md5]))),
+                ", ".join(set(md5_filenames[md5])),
+            )
 
     print(table)
 
