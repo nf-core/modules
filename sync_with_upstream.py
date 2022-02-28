992d7be899696476c8ef8718debf8da448051032
Matthieu Muffato <mm49@sanger.ac.uk>
2022-02-28 17:16:06 +0000

    Added a dry-run mode

--- sync_with_upstream.py
+++ sync_with_upstream.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import subprocess
 import sys
@@ -7,6 +8,9 @@ import tempfile
 
 import tolit.slack
 
+script_description = """
+Script to merge the upstream nf-core 'master' branch onto our own nf-core-modules ('main' branch)
+"""
 
 GITHUB_SANGER_MODULES = 'git@github.com:sanger-tol/nf-core-modules.git'
 GITHUB_NFCORE_MODULES = 'git@github.com:nf-core/modules.git'
@@ -42,13 +46,19 @@ def run_and_check_command(args):
         print(proc.stdout)
 
 def main():
+    parser = argparse.ArgumentParser(description=script_description)
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    args = parser.parse_args()
     with tempfile.TemporaryDirectory() as tmpdirname:
         run_and_check_command(["git", "clone", "-o", SANGER_REMOTE_NAME, "-b", SANGER_BRANCH_NAME, GITHUB_SANGER_MODULES, tmpdirname])
         os.chdir(tmpdirname)
         run_and_check_command(["git", "remote", "add", NFCORE_REMOTE_NAME, GITHUB_NFCORE_MODULES])
         run_and_check_command(["git", "fetch", NFCORE_REMOTE_NAME])
         run_and_check_command(["git", "merge", "--ff", "--no-edit", f"{NFCORE_REMOTE_NAME}/{NFCORE_BRANCH_NAME}"])
-        run_and_check_command(["git", "push", SANGER_REMOTE_NAME])
+        if args.dry_run:
+            run_and_check_command(["git", "push", "-n", SANGER_REMOTE_NAME])
+        else:
+            run_and_check_command(["git", "push", SANGER_REMOTE_NAME])
 
 if __name__ == "__main__":
     main()
