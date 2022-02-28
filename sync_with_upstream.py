5a11acd7454562c7814650af69e2f3e8ccb9a08a
Matthieu Muffato <mm49@sanger.ac.uk>
2022-02-28 17:15:38 +0000

    Script to synchronise our repository with upstream

---
+++ sync_with_upstream.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+import tempfile
+
+import tolit.slack
+
+
+GITHUB_SANGER_MODULES = 'git@github.com:sanger-tol/nf-core-modules.git'
+GITHUB_NFCORE_MODULES = 'git@github.com:nf-core/modules.git'
+SANGER_REMOTE_NAME = 'sanger'
+NFCORE_REMOTE_NAME = 'upstream'
+SANGER_BRANCH_NAME = 'main'
+NFCORE_BRANCH_NAME = 'master'
+
+
+def run_and_check_command(args):
+    print("Running", args)
+    proc = subprocess.run(args, capture_output=True, text=True)
+    if proc.returncode:
+        msg_items = [f'The command `{" ".join(args)}` failed with return code {proc.returncode}']
+        if proc.stdout:
+            msg_items.extend([
+                '_stdout_:',
+                '```',
+                proc.stdout,
+                '```',
+            ])
+        if proc.stderr:
+            msg_items.extend([
+                '_stderr_:',
+                '```',
+                proc.stderr,
+                '```',
+            ])
+        tolit.slack.report_to_slack(os.environ["SLACK_WEBHOOK_PERSO"], "\n".join(msg_items))
+        sys.exit(1)
+    else:
+        print("OK")
+        print(proc.stdout)
+
+def main():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        run_and_check_command(["git", "clone", "-o", SANGER_REMOTE_NAME, "-b", SANGER_BRANCH_NAME, GITHUB_SANGER_MODULES, tmpdirname])
+        os.chdir(tmpdirname)
+        run_and_check_command(["git", "remote", "add", NFCORE_REMOTE_NAME, GITHUB_NFCORE_MODULES])
+        run_and_check_command(["git", "fetch", NFCORE_REMOTE_NAME])
+        run_and_check_command(["git", "merge", "--ff", "--no-edit", f"{NFCORE_REMOTE_NAME}/{NFCORE_BRANCH_NAME}"])
+        run_and_check_command(["git", "push", SANGER_REMOTE_NAME])
+
+if __name__ == "__main__":
+    main()
