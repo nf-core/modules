6a1cbce0f538466fd3152a17217f71289417d91c
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-06-10 12:54:08 +0200

    Update tests/test_versions_yml.py
    Co-authored-by: Fabian Egli <fabianegli@users.noreply.github.com>

--- tests/test_versions_yml.py
+++ tests/test_versions_yml.py
@@ -56,5 +56,5 @@ def test_ensure_valid_version_yml(workflow_dir):
     assert len(software_versions), "There must be at least one version emitted."
     for tool, version in software_versions.items():
         assert re.match(
-            r"(^\d+.*|^[a-z0-9]{40}$)", str(version)
+            r"(^\d+.*|^[a-f0-9]{40}$)", str(version)
         ), f"Version number for {tool} must start with a number. "
