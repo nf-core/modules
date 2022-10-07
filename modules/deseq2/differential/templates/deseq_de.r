0f8e48efb62cad1b809859632c158334fc71b85b
Jonathan Manning <jonathan.manning@healx.io>
2022-10-07 12:28:50 +0100

    Try to appease eclint

--- modules/deseq2/differential/templates/deseq_de.r
+++ modules/deseq2/differential/templates/deseq_de.r
@@ -37,13 +37,13 @@ opt <- list(
 )
 
 for (file_input in c('count_file', 'sample_file')){
-  if (is.null(opt[[file_input]])) {
-    stop(paste("Please provide", file_input), call. = FALSE)
-  }
+    if (is.null(opt[[file_input]])) {
+        stop(paste("Please provide", file_input), call. = FALSE)
+    }
 
-  if (! file.exists(opt[[file_input]])){
-    stop(paste0('Value of ', file_input, ': ', opt[[file_input]], ' is not a valid file'))
-  }
+    if (! file.exists(opt[[file_input]])){
+        stop(paste0('Value of ', file_input, ': ', opt[[file_input]], ' is not a valid file'))
+    }
 }
 
 
@@ -62,11 +62,11 @@ library(DESeq2)
 ################################################
 
 count.table <-
-  read.delim(
-    file = opt\$count_file,
-    header = TRUE,
-    row.names = opt\$gene_id_col
-  )
+    read.delim(
+        file = opt\$count_file,
+        header = TRUE,
+        row.names = opt\$gene_id_col
+    )
 sample.sheet <- read.csv(file = opt\$sample_file)
 
 if (! opt\$sample_id_col %in% colnames(sample.sheet)){
@@ -84,19 +84,19 @@ rownames(sample.sheet) <- sample.sheet[[opt\$sample_id_col]]
 # sheet
 
 missing_samples <-
-  sample.sheet[!rownames(sample.sheet) %in% colnames(count.table), opt\$sample_id_col]
+    sample.sheet[!rownames(sample.sheet) %in% colnames(count.table), opt\$sample_id_col]
 
 if (length(missing_samples) > 0) {
-  stop(paste(
-    len(missing_samples),
-    'specified samples missing from count table:',
-    paste(missing_samples, collapse = ',')
-  ))
+    stop(paste(
+        len(missing_samples),
+        'specified samples missing from count table:',
+        paste(missing_samples, collapse = ',')
+    ))
 } else{
-  # Save any non-count data, will gene metadata etc we might need later
-  noncount.table <-
-    count.table[, !colnames(count.table) %in% rownames(sample.sheet), drop = FALSE]
-  count.table <- count.table[, rownames(sample.sheet)]
+    # Save any non-count data, will gene metadata etc we might need later
+    noncount.table <-
+        count.table[, !colnames(count.table) %in% rownames(sample.sheet), drop = FALSE]
+    count.table <- count.table[, rownames(sample.sheet)]
 }
 
 ################################################
@@ -108,32 +108,32 @@ if (length(missing_samples) > 0) {
 blocking.vars <- c()
 
 if (!opt\$contrast_variable %in% colnames(sample.sheet)) {
-  stop(
-    paste0(
-    'Chosen contrast variable \"',
-    opt\$contrast_variable,
-    '\" not in sample sheet'
-    )
-  )
-} else if (any(!c(opt\$reflevel, opt\$treatlevel) %in% sample.sheet[[opt\$contrast_variable]])) {
-  stop(
-    paste(
-    'Please choose reference and treatment levels that are present in the',
-    opt\$contrast_variable,
-    'column of the sample sheet'
-    )
-  )
-} else if (!is.null(opt\$blocking)) {
-  blocking.vars = unlist(strsplit(opt\$blocking, split = ','))
-  if (!all(blocking.vars %in% colnames(sample.sheet))) {
     stop(
-    paste0(
-        'One or more of the blocking variables specified (',
-        opt\$blocking,
-        ') do not correspond to sample sheet columns.'
+        paste0(
+        'Chosen contrast variable \"',
+        opt\$contrast_variable,
+        '\" not in sample sheet'
+        )
     )
+} else if (any(!c(opt\$reflevel, opt\$treatlevel) %in% sample.sheet[[opt\$contrast_variable]])) {
+    stop(
+        paste(
+        'Please choose reference and treatment levels that are present in the',
+        opt\$contrast_variable,
+        'column of the sample sheet'
+        )
     )
-  }
+} else if (!is.null(opt\$blocking)) {
+    blocking.vars = unlist(strsplit(opt\$blocking, split = ','))
+    if (!all(blocking.vars %in% colnames(sample.sheet))) {
+        stop(
+            paste0(
+                'One or more of the blocking variables specified (',
+                opt\$blocking,
+                ') do not correspond to sample sheet columns.'
+            )
+        )
+    }
 }
 
 # Now specify the model. Use cell-means style so we can be explicit with the
@@ -142,13 +142,13 @@ if (!opt\$contrast_variable %in% colnames(sample.sheet)) {
 model <- '~ 0 +'
 
 if (!is.null(opt\$blocking_variables)) {
-  model <- paste(model, paste(blocking.vars, collapse = '+'))
+    model <- paste(model, paste(blocking.vars, collapse = '+'))
 }
 
 # Make sure all the appropriate variables are factors
 
 for (v in c(blocking.vars, opt\$contrast_variable)) {
-  sample.sheet[[v]] <- as.factor(sample.sheet[[v]])
+    sample.sheet[[v]] <- as.factor(sample.sheet[[v]])
 }
 
 # Variable of interest goes last, see https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#multi-factor-designs
@@ -162,34 +162,34 @@ model <- paste(model, opt\$contrast_variable, sep = ' + ')
 ################################################
 
 dds <- DESeqDataSetFromMatrix(
-  countData = round(count.table),
-  colData = sample.sheet,
-  design = as.formula(model)
+    countData = round(count.table),
+    colData = sample.sheet,
+    design = as.formula(model)
 )
 
 dds <- DESeq(
-  dds,
-  test = opt\$test,
-  fitType = opt\$fit_type,
-  minReplicatesForReplace = opt\$min_replicates_for_replace,
-  useT = opt\$use_t,
-  sfType = opt\$sf_type
+    dds,
+    test = opt\$test,
+    fitType = opt\$fit_type,
+    minReplicatesForReplace = opt\$min_replicates_for_replace,
+    useT = opt\$use_t,
+    sfType = opt\$sf_type
 )
 
 comp.results <-
-  results(
-    dds,
-    lfcThreshold = opt\$lfc_threshold,
-    altHypothesis = opt\$alt_hypothesis,
-    independentFiltering = opt\$independent_filtering,
-    alpha = opt\$alpha,
-    pAdjustMethod = opt\$p_adjust_method,
-    minmu = opt\$minmu,
-    contrast = c(
-    opt\$contrast_variable,
-    c(opt\$treatment_level, opt\$reference_level)
+    results(
+        dds,
+        lfcThreshold = opt\$lfc_threshold,
+        altHypothesis = opt\$alt_hypothesis,
+        independentFiltering = opt\$independent_filtering,
+        alpha = opt\$alpha,
+        pAdjustMethod = opt\$p_adjust_method,
+        minmu = opt\$minmu,
+        contrast = c(
+            opt\$contrast_variable,
+            c(opt\$treatment_level, opt\$reference_level)
+        )
     )
-  )
 
 ################################################
 ################################################
@@ -198,7 +198,7 @@ comp.results <-
 ################################################
 
 contrast.name <-
-  paste(opt\$treatment_level, opt\$reference_level, sep = "_vs_")
+    paste(opt\$treatment_level, opt\$reference_level, sep = "_vs_")
 cat("Saving results for ", contrast.name, " ...\n", sep = "")
 
 if (!file.exists(opt\$outdir)) {
@@ -208,20 +208,20 @@ if (!file.exists(opt\$outdir)) {
 # Differential expression table
 
 write.table(
-  data.frame(gene_id = rownames(comp.results), comp.results),
-  file = file.path(opt\$outdir, 'deseq2.results.tsv'),
-  col.names = TRUE,
-  row.names = FALSE,
-  sep = '\t',
-  quote = FALSE
+    data.frame(gene_id = rownames(comp.results), comp.results),
+    file = file.path(opt\$outdir, 'deseq2.results.tsv'),
+    col.names = TRUE,
+    row.names = FALSE,
+    sep = '\t',
+    quote = FALSE
 )
 
 # Dispersion plot
 
 png(
-  file.path(opt\$outdir, 'deseq2.dispersion.png'),
-  width = 600,
-  height = 600
+    file.path(opt\$outdir, 'deseq2.dispersion.png'),
+    width = 600,
+    height = 600
 )
 plotDispEsts(dds)
 dev.off()
@@ -235,41 +235,41 @@ save(dds, file = file.path(opt\$outdir, 'dds.rld.RData'))
 sf_df = data.frame(sample = names(sizeFactors(dds)), data.frame(sizeFactors(dds)))
 colnames(sf_df) <- c('sample', 'sizeFactor')
 write.table(
-  sf_df,
-  file = file.path(opt\$outdir, 'deseq2.sizefactors.tsv'),
-  col.names = TRUE,
-  row.names = FALSE,
-  sep = '\t',
-  quote = FALSE
+    sf_df,
+    file = file.path(opt\$outdir, 'deseq2.sizefactors.tsv'),
+    col.names = TRUE,
+    row.names = FALSE,
+    sep = '\t',
+    quote = FALSE
 )
 
 # Write specified matrices
 
 if (opt\$write_normalised){
-  write.table(
-    data.frame(gene_id=rownames(counts(dds)), counts(dds, normalized = TRUE)),
-    file = file.path(opt\$outdir, 'normalised_counts.tsv'),
-    col.names = TRUE,
-    row.names = FALSE,
-    sep = '\t',
-    quote = FALSE
-  )
+    write.table(
+        data.frame(gene_id=rownames(counts(dds)), counts(dds, normalized = TRUE)),
+        file = file.path(opt\$outdir, 'normalised_counts.tsv'),
+        col.names = TRUE,
+        row.names = FALSE,
+        sep = '\t',
+        quote = FALSE
+    )
 }
 
 if (opt\$write_variance_stabilised){
-  if (opt\$vs_method == 'vst'){
-    vs_func = vst
-  }else{
-    vs_func = rlog
-  }
-  write.table(
-    data.frame(gene_id=rownames(counts(dds)), assay(vs_func(dds))),
-    file = file.path(opt\$outdir, 'variance_stabilised_counts.tsv'),
-    col.names = TRUE,
-    row.names = FALSE,
-    sep = '\t',
-    quote = FALSE
-  )
+    if (opt\$vs_method == 'vst'){
+        vs_func = vst
+    }else{
+        vs_func = rlog
+    }
+    write.table(
+        data.frame(gene_id=rownames(counts(dds)), assay(vs_func(dds))),
+        file = file.path(opt\$outdir, 'variance_stabilised_counts.tsv'),
+        col.names = TRUE,
+        row.names = FALSE,
+        sep = '\t',
+        quote = FALSE
+    )
 }
 
 ################################################
