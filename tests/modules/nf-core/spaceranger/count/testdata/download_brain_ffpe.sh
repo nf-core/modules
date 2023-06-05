f47f8a0d96db0dee887651caba29bd076804f4ee
Gregor Sturm <gregor.sturm@boehringer-ingelheim.com>
2023-06-05 08:24:33 +0200

    Remove test datasets
    The moved to nf-core/testdata

--- tests/modules/nf-core/spaceranger/count/testdata/download_brain_ffpe.sh
+++
@@ -1,8 +0,0 @@
-DIR=human-brain-cancer-11-mm-capture-area-ffpe-2-standard_v2_ffpe_cytassist
-mkdir -p $DIR && cd $DIR
-# Input Files
-curl -O https://s3-us-west-2.amazonaws.com/10x.files/samples/spatial-exp/2.0.1/CytAssist_11mm_FFPE_Human_Glioblastoma/CytAssist_11mm_FFPE_Human_Glioblastoma_fastqs.tar
-curl -O https://cf.10xgenomics.com/samples/spatial-exp/2.0.1/CytAssist_11mm_FFPE_Human_Glioblastoma/CytAssist_11mm_FFPE_Human_Glioblastoma_image.tif
-curl -O https://cf.10xgenomics.com/samples/spatial-exp/2.0.1/CytAssist_11mm_FFPE_Human_Glioblastoma/CytAssist_11mm_FFPE_Human_Glioblastoma_probe_set.csv
-curl -O https://cf.10xgenomics.com/samples/spatial-exp/2.0.1/CytAssist_11mm_FFPE_Human_Glioblastoma/CytAssist_11mm_FFPE_Human_Glioblastoma_tissue_image.tif
-
