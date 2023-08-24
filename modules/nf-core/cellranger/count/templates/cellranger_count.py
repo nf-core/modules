797d02b5cfafed93be70bbbb9dcc4cedc59399b0
Gregor Sturm <gregor.sturm@boehringer-ingelheim.com>
2023-08-24 13:03:14 +0200

    fix editorconfig

--- modules/nf-core/cellranger/count/templates/cellranger_count.py
+++ modules/nf-core/cellranger/count/templates/cellranger_count.py
@@ -27,8 +27,8 @@ fastq_all = Path("./fastq_all")
 fastq_all.mkdir(exist_ok=True)
 
 # Match R1 in the filename, but only if it is followed by a non-digit or non-character
-# match "file_R1.fastq.gz", "file.R1_000.fastq.gz", etc. but 
-# do not match "SRR12345", "file_INFIXR12", etc  
+# match "file_R1.fastq.gz", "file.R1_000.fastq.gz", etc. but
+# do not match "SRR12345", "file_INFIXR12", etc
 filename_pattern =  r'([^a-zA-Z0-9])R1([^a-zA-Z0-9])'
 
 for i, (r1, r2) in enumerate(chunk_iter(fastqs, 2)):
@@ -37,9 +37,9 @@ for i, (r1, r2) in enumerate(chunk_iter(fastqs, 2)):
             dedent(
                 f"""\
                 We expect R1 and R2 of the same sample to have the same filename except for R1/R2.
-                This has been checked by replacing "R1" with "R2" in the first filename and comparing it to the second filename. 
+                This has been checked by replacing "R1" with "R2" in the first filename and comparing it to the second filename.
                 If you believe this check shouldn't have failed on your filenames, please report an issue on GitHub!
-                
+
                 Files involved:
                     - {r1}
                     - {r2}
