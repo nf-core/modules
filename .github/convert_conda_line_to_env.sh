18ac73f9a98b57faab26a1f9c9676bfe4ea5acc7
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-24 04:36:46 +0200

    Fix updating the conda line

--- .github/convert_conda_line_to_env.sh
+++ .github/convert_conda_line_to_env.sh
@@ -12,11 +12,13 @@ for file in $NF_FILES; do
     conda_packages=$(echo "$conda_line" | sed 's/^.*"\(.*\)".*$/\1/g')
     # Write the template to the environment.yml
     cat $template > $(dirname "$file")/environment.yml
+
+    env_yaml=$(dirname "$file")/environment.yml
     # Put the dependencies in a environment.yaml using yq
     for package in $conda_packages; do
-        yq -i '.dependencies += ["'"$package"'"]' $(dirname "$file")/environment.yml
+        yq -i '.dependencies += ["'"$package"'"]' $env_yaml
     done
 
-    # Change the conda line to environment.yml
-    # sed -i 's/^    conda.*$/^    conda $(dirname "$file")/environment.yml/g' "$file"
+    # Change the conda line to point to the new environment.yml
+    sed -i "s|^    conda.*|    conda '$env_yaml'|g" "$file"
 done
