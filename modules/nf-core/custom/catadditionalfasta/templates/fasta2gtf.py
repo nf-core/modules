aa170b37c60189edba1b07729a50bf6971e4ed15
Jonathan Manning <jonathan.manning@seqera.io>
2023-12-20 20:18:32 +0000

    Update meta.yml, fix linting

--- modules/nf-core/custom/catadditionalfasta/templates/fasta2gtf.py
+++ modules/nf-core/custom/catadditionalfasta/templates/fasta2gtf.py
@@ -8,6 +8,7 @@ import platform
 from typing import Iterator, Tuple
 from itertools import groupby
 
+
 def setup_logging() -> logging.Logger:
     """Configure logging for the script.
 
@@ -18,6 +19,7 @@ def setup_logging() -> logging.Logger:
     logger = logging.getLogger(__file__)
     logger.setLevel(logging.INFO)
     return logger
+
 
 def format_yaml_like(data: dict, indent: int = 0) -> str:
     """Formats a dictionary to a YAML-like string.
@@ -38,6 +40,7 @@ def format_yaml_like(data: dict, indent: int = 0) -> str:
             yaml_str += f"{spaces}{key}: {value}\\n"
     return yaml_str
 
+
 def parse_fasta(fasta_file: str) -> Iterator[Tuple[str, str]]:
     """Parse a fasta file and yield tuples of header and sequence.
 
@@ -46,7 +49,7 @@ def parse_fasta(fasta_file: str) -> Iterator[Tuple[str, str]]:
 
     Yields:
         Iterator[Tuple[str, str]]: Tuples of header and sequence from the fasta file.
-    
+
 
     modified from Brent Pedersen
     Correct Way To Parse A Fasta File In Python
@@ -61,10 +64,11 @@ def parse_fasta(fasta_file: str) -> Iterator[Tuple[str, str]]:
             sequence = "".join(s.strip() for s in next(fasta_iter))
             yield (header_str, sequence)
 
+
 def fasta_to_gtf(fasta: str, output_file: str, biotype: str) -> None:
     """
     Read a fasta file and create a GTF file.
-    
+
     Args:
         fasta (str): Path to the fasta file.
         output_file (str): Path for the output GTF file.
@@ -81,6 +85,7 @@ def fasta_to_gtf(fasta: str, output_file: str, biotype: str) -> None:
     with open(output_file, "w") as file_handle:
         file_handle.writelines(lines)
 
+
 def generate_gtf_line(name: str, length: int, biotype: str) -> str:
     """Generate a single GTF line given sequence name, length, and biotype.
 
@@ -93,8 +98,9 @@ def generate_gtf_line(name: str, length: int, biotype: str) -> str:
         str: A formatted GTF line.
     """
     biotype_attr = f' {biotype} "transgene";' if biotype else ""
-    attributes = f'exon_id "{name}.1"; exon_number "1";{biotype_attr} gene_id "{name}_gene"; gene_name "{name}_gene"; gene_source "custom"; transcript_id "{name}_gene"; transcript_name "{name}_gene";\\n'    
+    attributes = f'exon_id "{name}.1"; exon_number "1";{biotype_attr} gene_id "{name}_gene"; gene_name "{name}_gene"; gene_source "custom"; transcript_id "{name}_gene"; transcript_name "{name}_gene";\\n'
     return f"{name}\\ttransgene\\texon\\t1\\t{length}\\t.\\t+\\t.\\t{attributes}"
+
 
 def main() -> None:
     # Parse arguments using argparse (not shown for brevity)
@@ -106,24 +112,31 @@ def main() -> None:
     # Add fasta lines to GTF
     add_name = os.path.splitext(os.path.basename("$add_fasta"))[0]
     fasta_to_gtf("$add_fasta", f"{add_name}.gtf", "$biotype")
-    
-    # Concatenate new fasta to existing fasta, and the GTF we just generated to the GTF
-    genome_name  = "$params.genome" if "$params.genome" != 'null' else os.path.splitext(os.path.basename("$fasta"))[0]
-    output_prefix = '$task.ext.prefix' if '$task.ext.prefix' != 'null' else f"{genome_name}_{add_name}"
 
-    os.mkdir('out')
+    # Concatenate new fasta to existing fasta, and the GTF we just generated to the GTF
+    genome_name = (
+        "$params.genome"
+        if "$params.genome" != "null"
+        else os.path.splitext(os.path.basename("$fasta"))[0]
+    )
+    output_prefix = (
+        "$task.ext.prefix"
+        if "$task.ext.prefix" != "null"
+        else f"{genome_name}_{add_name}"
+    )
+
+    os.mkdir("out")
     os.system(f"cat $fasta $add_fasta > out/{output_prefix}.fasta")
     os.system(f"cat $gtf {add_name}.gtf > out/{output_prefix}.gtf")
-    
+
     logger.info("Conversion completed successfully.")
-    
+
     # Write the versions
     versions_this_module = {}
-    versions_this_module["${task.process}"] = {
-        "python": platform.python_version()
-    }
+    versions_this_module["${task.process}"] = {"python": platform.python_version()}
     with open("versions.yml", "w") as f:
         f.write(format_yaml_like(versions_this_module))
+
 
 if __name__ == "__main__":
     main()
