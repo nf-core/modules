0faf8032f6565e50cd38c53c536f372f5ca8601c
Irene Robles Rebollo <irenerobles93@gmail.com>
2024-03-19 17:20:49 +0000

    feat: more tests

--- modules/nf-core/scsplit/count/test_data.sh
+++ modules/nf-core/scsplit/count/test_data.sh
@@ -7,6 +7,8 @@ wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1jlhE
 FILEID="13CV6CjP9VzmwG5MVHbJiVDMVdiIhGdJB"
 FILENAME="jurkat_293t_downsampled_n500_full_bam.bam"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+wget --no-check-certificate https://figshare.com/ndownloader/files/41773428 -O barcodes.tsv
+
 
 gunzip jurkat_293t_exons_only.vcf.withAF.vcf.gz
 
@@ -17,3 +19,5 @@ wget --no-check-certificate https://figshare.com/ndownloader/files/43102453 -O g
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lw4T6d7uXsm9dt39ZtEwpuB2VTY3wK1y' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lw4T6d7uXsm9dt39ZtEwpuB2VTY3wK1y" -O common_variants_hg19.vcf && rm -rf /tmp/cookies.txt
 wget https://master.dl.sourceforge.net/project/cellsnp/SNPlist/genome1K.phase3.SNP_AF5e2.chr1toX.hg19.vcf.gz
+
+freebayes -f genome_chr1.fa jurkat_293t_downsampled_n500_full_bam.bam > snv.vcf
