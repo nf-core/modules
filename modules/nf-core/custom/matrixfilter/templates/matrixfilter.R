fa7e3760db642a1afd6081f21b25e8b6b9ca8680
tom.giles <tom.giles@nottingham.ac.uk>
2024-03-21 00:59:58 +0000

    Added variant filtering to matrix filter

--- modules/nf-core/custom/matrixfilter/templates/matrixfilter.R
+++ modules/nf-core/custom/matrixfilter/templates/matrixfilter.R
@@ -72,30 +72,31 @@ read_delim_flexible <- function(file, header = TRUE, row.names = NULL, nrows = -
 # Set up default options
 
 opt <- list(
-    abundance_matrix_file = '$abundance',
-    sample_file = '$samplesheet',
-    sample_id_col = NULL,
-    minimum_abundance = 1,
-    minimum_samples = 1,
-    minimum_proportion = 0,
-    grouping_variable = NULL,
-    minimum_proportion_not_na = 0.5,
-    minimum_samples_not_na = NULL
+  abundance_matrix_file = '$abundance',
+  sample_file = '$samplesheet',
+  sample_id_col = NULL,
+  minimum_abundance = 1,
+  minimum_samples = 1,
+  minimum_proportion = 0,
+  grouping_variable = NULL,
+  minimum_proportion_not_na = 0.5,
+  minimum_samples_not_na = NULL,
+  most_variant_features = NULL
 )
 opt_types <- lapply(opt, class)
 
 args_opt <- parse_args('$task.ext.args')
 for ( ao in names(args_opt)){
-    if (! ao %in% names(opt)){
-        stop(paste("Invalid option:", ao))
-    }else{
-
-        # Preserve classes from defaults where possible
-        if (! is.null(opt[[ao]])){
-            args_opt[[ao]] <- as(args_opt[[ao]], opt_types[[ao]])
-        }
-        opt[[ao]] <- args_opt[[ao]]
+  if (! ao %in% names(opt)){
+    stop(paste("Invalid option:", ao))
+  }else{
+    
+    # Preserve classes from defaults where possible
+    if (! is.null(opt[[ao]])){
+      args_opt[[ao]] <- as(args_opt[[ao]], opt_types[[ao]])
     }
+    opt[[ao]] <- args_opt[[ao]]
+  }
 }
 
 abundance_matrix <- read_delim_flexible(opt\$abundance_matrix_file, row.names = 1)
@@ -104,54 +105,54 @@ feature_id_name <- colnames( read_delim_flexible(opt\$abundance_matrix_file, nro
 # If a sample sheet was specified, validate the matrix against it
 
 if (opt\$sample_file != ''){
-
-    # Read the sample sheet and check against matrix
-
-    samplesheet <- read_delim_flexible(
-        opt\$sample_file,
-        row.names = ifelse(is.null(opt\$sample_id_col), 1, opt\$sample_id_col)
+  
+  # Read the sample sheet and check against matrix
+  
+  samplesheet <- read_delim_flexible(
+    opt\$sample_file,
+    row.names = ifelse(is.null(opt\$sample_id_col), 1, opt\$sample_id_col)
+  )
+  missing_samples <- setdiff(rownames(samplesheet), colnames(abundance_matrix))
+  
+  if (length(missing_samples) > 0){
+    stop(
+      paste(
+        paste(missing_samples, collapse = ', '),
+        'not represented in supplied abundance matrix'
+      )
     )
-    missing_samples <- setdiff(rownames(samplesheet), colnames(abundance_matrix))
-
-    if (length(missing_samples) > 0){
-        stop(
-            paste(
-                paste(missing_samples, collapse = ', '),
-                'not represented in supplied abundance matrix'
-            )
-        )
-    }else{
-        abundance_matrix <- abundance_matrix[,rownames(samplesheet)]
-    }
+  }else{
+    abundance_matrix <- abundance_matrix[,rownames(samplesheet)]
+  }
 }else{
-
-    # If we're not using a sample sheet to select columns, then at least make
-    # sure the ones we have are numeric (some upstream things like the RNA-seq
-    # workflow have annotation colummns as well)
-
-    numeric_columns <- unlist(lapply(1:ncol(abundance_matrix), function(x) is.numeric(abundance_matrix[,x])))
-    abundance_matrix <- abundance_matrix[,numeric_columns]
+  
+  # If we're not using a sample sheet to select columns, then at least make
+  # sure the ones we have are numeric (some upstream things like the RNA-seq
+  # workflow have annotation colummns as well)
+  
+  numeric_columns <- unlist(lapply(1:ncol(abundance_matrix), function(x) is.numeric(abundance_matrix[,x])))
+  abundance_matrix <- abundance_matrix[,numeric_columns]
 }
 
 # If we want to define n based on the levels of a grouping variable...
 
 if ((opt\$sample_file != '') && ( ! is.null(opt\$grouping_variable))){
-
-    # Pick a minimum number of samples to pass threshold based on group size
-
-    if (! opt\$grouping_variable %in% colnames(samplesheet)){
-        stop(paste(opt\$grouping_variable, "not in supplied sample sheet"))
-    }else{
-        opt\$minimum_samples <- min(table(samplesheet[[opt\$grouping_variable]]))
-        if ( opt\$minimum_proportion > 0){
-            opt\$minimum_samples <- opt\$minimum_samples * opt\$minimum_proportion
-        }
+  
+  # Pick a minimum number of samples to pass threshold based on group size
+  
+  if (! opt\$grouping_variable %in% colnames(samplesheet)){
+    stop(paste(opt\$grouping_variable, "not in supplied sample sheet"))
+  }else{
+    opt\$minimum_samples <- min(table(samplesheet[[opt\$grouping_variable]]))
+    if ( opt\$minimum_proportion > 0){
+      opt\$minimum_samples <- opt\$minimum_samples * opt\$minimum_proportion
     }
+  }
 }else if (opt\$minimum_proportion > 0){
-
-    # Or if we want to define it based on a static proportion of the sample number
-
-    opt\$minimum_samples <- ncol(abundance_matrix) * opt\$minimum_proportion
+  
+  # Or if we want to define it based on a static proportion of the sample number
+  
+  opt\$minimum_samples <- ncol(abundance_matrix) * opt\$minimum_proportion
 }
 
 # Also set up filtering for NAs; use by default minimum_proportion_not_na; only
@@ -160,55 +161,67 @@ if ((opt\$sample_file != '') && ( ! is.null(opt\$grouping_variable))){
 # value even if the proportion is to be used
 
 if (is.null(opt\$minimum_samples_not_na)) {
-    opt\$minimum_samples_not_na <- ncol(abundance_matrix) * opt\$minimum_proportion_not_na
+  opt\$minimum_samples_not_na <- ncol(abundance_matrix) * opt\$minimum_proportion_not_na
+}
+
+# We can assume that in most cases no variant filter is required 
+# if no variant filter is set we will assume that the n filter threshold is equivlent to the row lenght
+if (is.null(opt\$most_variant_features)) {
+  opt\$most_variant_features <- nrow(abundance_matrix)
 }
 
 # Define the tests
-
 tests <- list(
-    'abundance' = function(x) sum(x >= opt\$minimum_abundance, na.rm = T) >= opt\$minimum_samples, # check if rows have sufficiently high abundance
-    'na' = function(x) !any(is.na(x)) || sum(!is.na(x)) >= opt\$minimum_samples_not_na  # check if enough values in row are not NA
+  'abundance' = function(x) sum(x >= opt\$minimum_abundance, na.rm = TRUE) >= opt\$minimum_samples, # check if rows have sufficiently high abundance
+  'na' = function(x) !any(is.na(x)) || sum(!is.na(x)) >= opt\$minimum_samples_not_na  # check if enough values in row are not NA
 )
 
-# Apply the functions row-wise on the abundance_matrix and store the result in a boolean matrix
+# Function to identify rows that are among the top n most variant
+most_variant_test <- function(matrix_data) {
+  # Determine the indices of the top variant rows based on variance
+  top_indices <- order(-apply(matrix_data, 1, var, na.rm = TRUE))[1:opt\$most_variant_features]
+  # Return a boolean vector indicating if each row is among the top variant ones
+  1:nrow(matrix_data) %in% top_indices
+}
 
+# Apply the 'abundance' and 'na' tests row-wise to the abundance_matrix
 boolean_matrix <- t(apply(abundance_matrix, 1, function(row) {
-    sapply(tests, function(f) f(row))
+  sapply(tests, function(f) f(row))
 }))
 
-# We will retain features passing all tests
+# Apply the 'most_variant_test' function to identify the most variant rows and add the result to the boolean matrix
+most_variant_vector <- most_variant_test(abundance_matrix)
+boolean_matrix <- cbind(boolean_matrix, most_variant_vector)
 
-keep <- apply(boolean_matrix, 1, all)
-
-# Write out the matrix retaining the specified rows and re-prepending the
-# column with the feature identifiers
+# Determine rows to keep by checking if all tests are passed for each row
+keep <- apply(boolean_matrix, 1, all) # column with the feature identifiers
 
 prefix = ifelse('$task.ext.prefix' == 'null', '', '$task.ext.prefix')
 
 write.table(
-    data.frame(rownames(abundance_matrix)[keep], abundance_matrix[keep,,drop = FALSE]),
-    file = paste0(
-        prefix,
-        '.filtered.tsv'
-    ),
-    col.names = c(feature_id_name, colnames(abundance_matrix)),
-    row.names = FALSE,
-    sep = '\t',
-    quote = FALSE
+  data.frame(rownames(abundance_matrix)[keep], abundance_matrix[keep,,drop = FALSE]),
+  file = paste0(
+    prefix,
+    '.filtered.tsv'
+  ),
+  col.names = c(feature_id_name, colnames(abundance_matrix)),
+  row.names = FALSE,
+  sep = '\t',
+  quote = FALSE
 )
 
 # Write a boolean matrix returning specifying the status of each test
 
 write.table(
-    data.frame(rownames(abundance_matrix), boolean_matrix),
-    file = paste0(
-        prefix,
-        '.tests.tsv'
-    ),
-    col.names = c(feature_id_name, names(tests)),
-    row.names = FALSE,
-    sep = '\t',
-    quote = FALSE
+  data.frame(rownames(abundance_matrix), boolean_matrix),
+  file = paste0(
+    prefix,
+    '.tests.tsv'
+  ),
+  col.names = c(feature_id_name, names(tests)),
+  row.names = FALSE,
+  sep = '\t',
+  quote = FALSE
 )
 
 ################################################
@@ -230,11 +243,11 @@ sink()
 r.version <- strsplit(version[['version.string']], ' ')[[1]][3]
 
 writeLines(
-    c(
-        '"${task.process}":',
-        paste('    r-base:', r.version)
-    ),
-'versions.yml')
+  c(
+    '"${task.process}":',
+    paste('    r-base:', r.version)
+  ),
+  'versions.yml')
 
 ################################################
 ################################################
