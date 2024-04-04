248ebd698eb3215c15d1b1e5f99b8c7f4d49161e
Sebastian Uhrig <suhrig@altoslabs.com>
2024-04-04 19:43:19 +0000

    long options

--- modules/nf-core/sratools/prefetch/templates/retry_with_backoff.sh
+++ modules/nf-core/sratools/prefetch/templates/retry_with_backoff.sh
@@ -50,9 +50,9 @@ retry_with_backoff !{args2} \
 # check file integrity using vdb-validate or (when archive contains no checksums) md5sum
 vdb-validate !{id} > vdb-validate_result.txt 2>&1 || exit 1
 if grep -q "checksums missing" vdb-validate_result.txt; then
-	VALID_MD5SUMS=$(curl -s -f -L --retry 3 --retry-delay 60 'https://locate.ncbi.nlm.nih.gov/sdl/2/retrieve?filetype=run&acc=!{id}')
+	VALID_MD5SUMS=$(curl --silent --fail --location --retry 3 --retry-delay 60 'https://locate.ncbi.nlm.nih.gov/sdl/2/retrieve?filetype=run&acc=!{id}')
 	LOCAL_MD5SUMS=$(md5sum !{id}/* | cut -f1 -d' ')
-	grep -q -F -f <(echo "$LOCAL_MD5SUMS") <(echo "$VALID_MD5SUMS") || exit 1
+	grep --quiet --fixed-strings --file=<(echo "$LOCAL_MD5SUMS") <(echo "$VALID_MD5SUMS") || exit 1
 fi
 
 cat <<-END_VERSIONS > versions.yml
