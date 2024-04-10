ccbbbe4f9d8d94144255bf8db794be0078a4a888
WackerO <43847497+WackerO@users.noreply.github.com>
2024-04-10 11:12:28 +0200

    Revert "Fix module: proteus (#5442)"
    This reverts commit 4c7d5c1b9278cc66a155d3eb6fc0ec25657168ea.

--- modules/nf-core/proteus/readproteingroups/templates/proteus_readproteingroups.R
+++ modules/nf-core/proteus/readproteingroups/templates/proteus_readproteingroups.R
@@ -54,11 +54,10 @@ parse_args <- function(x) {
 #' @param file Input file
 #' @param header Passed to read.delim()
 #' @param row.names Passed to read.delim()
-#' @param nrows Passed to read.delim()
 #'
 #' @return output Data frame
 
-read_delim_flexible <- function(file, header = TRUE, row.names = NULL, check.names = F, nrows = -1) {
+read_delim_flexible <- function(file, header = TRUE, row.names = NULL, check.names = F) {
 
     ext <- tolower(tail(strsplit(basename(file), split = "\\\\.")[[1]], 1))
 
@@ -75,8 +74,7 @@ read_delim_flexible <- function(file, header = TRUE, row.names = NULL, check.nam
         sep = separator,
         header = header,
         row.names = row.names,
-        check.names = check.names,
-        nrows = nrows
+        check.names = check.names
     )
 }
 
@@ -194,8 +192,7 @@ library(proteus)
 intensities.table <-
     read_delim_flexible(
         file = opt\$intensities_file,
-        check.names = FALSE,
-        nrows = 1 # Here, we are only interested in the header of the table
+        check.names = FALSE
     )
 
 sample.sheet <-
@@ -221,19 +218,16 @@ sample.sheet\$condition <- sample.sheet[[opt\$contrast_variable]]
 
 measure.cols <- setNames(paste0(opt\$measure_col_prefix, sample.sheet[[opt\$sample_id_col]]), sample.sheet[[opt\$sample_id_col]])
 
-if (!any(measure.cols %in% colnames(intensities.table))) {
-    measure.cols <- setNames(paste0(paste0(opt\$measure_col_prefix, " "), sample.sheet[[opt\$sample_id_col]]), sample.sheet[[opt\$sample_id_col]])
-}
-
 # Check that all samples specified in the input sheet are present in the intensities table
 
-missing_columns <- measure.cols[!measure.cols %in% colnames(intensities.table)]
+missing_columns <- paste0(opt\$measure_col_prefix, sample.sheet[[opt\$sample_id_col]])
+missing_columns <- missing_columns[!missing_columns %in% colnames(intensities.table)]
 if (length(missing_columns) > 0) {
     stop(paste(
         length(missing_columns),
         'specified samples do not have a(n)',
         opt\$measure_col_prefix,
-        'column in intensities table (tried prefix both with and without adding a whitespace), please check the value of parameter --measure_col_prefix. The following columns are missing:',
+        'column in intensities table. The following columns are missing:',
         paste(missing_columns, collapse = ', ')
     ))
 }
