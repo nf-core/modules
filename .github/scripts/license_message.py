67a857b850ca06de42fb55107f9d1d57e93b4c8f
Phil Ewels <phil.ewels@seqera.io>
2024-09-06 10:09:40 +0200

    Ruff lint: DecryptionTimeout -> DecryptionTimeoutError

--- .github/scripts/license_message.py
+++ .github/scripts/license_message.py
@@ -20,7 +20,7 @@ MESSAGE_TIMEOUT = 60 * 60 * 24  # Messages are valid for 1 day
 NONCE_BYTES = 12
 
 
-class DecryptionTimeout(Exception):
+class DecryptionTimeoutError(Exception):
     # Decrypting a message that is too old
     pass
 
@@ -60,7 +60,7 @@ def decrypt_message(key, ciphertext, timeout=MESSAGE_TIMEOUT):
     msg_timestamp = int.from_bytes(msg_timestamp, byteorder="big")
     timestamp = calendar.timegm(dt.now().utctimetuple())
     if (timestamp - msg_timestamp) > timeout:
-        raise DecryptionTimeout("The message has an expired timeout")
+        raise DecryptionTimeoutError("The message has an expired timeout")
     return message.decode("utf-8")
 
 
