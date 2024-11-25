978641cd7338b12a67f472758e63acf622872980
Edmund Miller <git@edmundmiller.dev>
2024-11-25 15:52:48 -0600

    feat: Update the script to actually work

--- .github/scripts/renovate_env_comments.py
+++ .github/scripts/renovate_env_comments.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 
-import os
+import argparse
 import re
-
-# Define the directories to search for environment.yml files
-directories = [
-    "modules/nf-core/bowtie/align",
-    "modules/nf-core/bowtie/build",
-    "modules/nf-core/samtools/view",
-    # Add more directories as needed
-]
+from pathlib import Path
+from typing import Optional, Sequence
 
 # Define a regex pattern to match dependencies
 dependency_pattern = re.compile(r"^\s*-\s*(?P<channel>[\w-]+)::(?P<package>[\w-]+)=?(?P<version>[\d\.]*)")
@@ -17,7 +11,7 @@ dependency_pattern = re.compile(r"^\s*-\s*(?P<channel>[\w-]+)::(?P<package>[\w-]
 
 # Function to add or verify renovate comments
 def process_environment_file(file_path):
-    with open(file_path, "r") as file:
+    with open(file_path) as file:
         lines = file.readlines()
 
     updated_lines = []
@@ -40,11 +34,15 @@ def process_environment_file(file_path):
         file.writelines(updated_lines)
 
 
-# Traverse directories and process each environment.yml file
-for directory in directories:
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file == "environment.yml":
-                file_path = os.path.join(root, file)
-                process_environment_file(file_path)
-                print(f"Processed {file_path}")
+def main(argv: Optional[Sequence[str]] = None) -> None:
+    """Add Renovate comments to dependencies in conda environment files."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("paths", nargs="*", type=Path)
+    args = parser.parse_args(argv)
+    for path in args.paths:
+        print(path)
+        process_environment_file(path)
+
+
+if __name__ == "__main__":
+    main()
