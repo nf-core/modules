bff204a30cf6f941823d4d9356d6805e37f836f7
Edmund Miller <git@edmundmiller.dev>
2024-11-27 10:07:53 -0600

    ci: Add second test case

--- .github/scripts/get_module_path.js
+++ .github/scripts/get_module_path.js
@@ -14,6 +14,7 @@ function get_module_names(filter_modules_files) {
     ];
 }
 
+// https://github.com/nf-core/modules/pull/7075
 test_case_1 = [
     "modules/nf-core/umicollapse/tests/main.nf.test",
     "modules/nf-core/umicollapse/tests/main.nf.test.snap",
@@ -27,4 +28,18 @@ result_1 = ["umicollapse", "umitools/dedup"];
 
 console.assert(get_module_names(test_case_1) === result_1, "%o", { "Test Case 1": get_module_names(test_case_1) });
 
-test_case_2 = ["modules/nf-core/umicollapse/tests/main.nf.test"];
+// https://github.com/nf-core/modules/actions/runs/12047961816/job/33591591596?pr=7097
+test_case_2 = [
+    "modules/nf-core/mafft/align/environment.yml",
+    "modules/nf-core/mafft/align/main.nf",
+    "modules/nf-core/mafft/align/meta.yml",
+    "modules/nf-core/mafft/align/tests/main.nf.test",
+    "modules/nf-core/mafft/align/tests/main.nf.test.snap",
+    "modules/nf-core/mafft/guidetree/environment.yml",
+    "modules/nf-core/mafft/guidetree/main.nf",
+    "modules/nf-core/mafft/guidetree/meta.yml",
+    "modules/nf-core/mafft/guidetree/tests/main.nf.test",
+    "modules/nf-core/mafft/guidetree/tests/main.nf.test.snap",
+    "tests/modules/nf-core/epang/split/main.nf",
+];
+result_2 = ["mafft/align", "mafft/guidetree", "epang/split"];
