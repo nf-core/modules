eeec7023dea228e5083d6bbeb2777575c7f00deb
nschcolnicov <nico.schcolnicov@gmail.com>
2024-11-27 20:23:41 +0000

    Minor fixes

--- modules/nf-core/create_gene_heatmap/resources/usr/bin/compute_gene_heatmap.R
+++ modules/nf-core/create_gene_heatmap/resources/usr/bin/compute_gene_heatmap.R
@@ -10,7 +10,7 @@ library(ragg)
 ###Command line argument parsing###
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) < 1) {
-    stop("Usage: compute_gene_heatmap.R <annotated_endo_data.tsv> <normalized_counts.tsv> <sample_id_col> or compute_gene_heatmap.R <annotated_counts.tsv> <counts.tsv> <genes.yaml> <sample_id_col>", call.=FALSE)
+    stop("Usage: compute_gene_heatmap.R <annotated_endo_data.tsv> <normalized_counts.tsv> <sample_id_col> or compute_gene_heatmap.R <annotated_endo_data.tsv> <normalized_counts.tsv> <heatmap_genes_to_filter.yaml> <sample_id_col>", call.=FALSE)
 }
 input_counts_annotated <- args[1]
 input_counts <- args[2]
