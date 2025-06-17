8d883e16478c94040a73aeab86ba87ba111e8efa
LuisHeinzlmeier <luis.heinzlmeier@tum.de>
2025-06-17 09:54:27 +0200

    all tests passed

--- modules/nf-core/bff/main.nf
+++ modules/nf-core/bff/main.nf
@@ -4,14 +4,15 @@ process BFF {
 
     conda "${moduleDir}/environment.yml"
     container "${ workflow.containerEngine == 'singularity' && !task.ext.singularity_pull_docker_container ?
-        'https://depot.galaxyproject.org/singularity/r-seurat:3.0.2--r36h0357c0b_0':
-        'biocontainers/r-seurat:3.0.2--r36h0357c0b_0' }"
+        'oras://community.wave.seqera.io/library/bioconductor-cellhashr_r-seurat:1c94360d8ed188c4':
+        'community.wave.seqera.io/library/bioconductor-cellhashr_r-seurat:25c4bc76749af5ac' }"
 
     input:
     tuple val(meta), path(hto_matrix), val(methods), val(preprocessing)
 
     output:
-    tuple val(meta), path("*_assignment_bff.csv")       , emit: results
+    tuple val(meta), path("*_assignment_bff.csv")       , emit: assignment
+    tuple val(meta), path("*_metrics_bff.csv")          , emit: metrics
     tuple val(meta), path("*_params_bff.csv")           , emit: params
     path "versions.yml"                                 , emit: versions
 
@@ -31,18 +32,20 @@ process BFF {
     chemistry = task.ext.chemistry ?: "10xV3"
     callerDisagreementThreshold = task.ext.callerDisagreementThreshold ?: "NULL"
 
-    template bff.R
+    template 'bff.R'
 
     stub:
     def prefix = task.ext.prefix ?: "${meta.id}"
     """
-
-    touch ${prefix}.bam
+    touch ${prefix}_assignment_bff.csv
+    touch ${prefix}_metrics_bff.csv
+    touch ${prefix}_params_bff.csv
 
     cat <<-END_VERSIONS > versions.yml
     "${task.process}":
         r-base: \$(Rscript -e "cat(strsplit(R.version[['version.string']], ' ')[[1]][3])")
-        r-propr: \$(Rscript -e "library('propr'); cat(as.character(packageVersion('propr')))")
+        r-seurat: \$(Rscript -e "library(Seurat); cat(as.character(packageVersion('Seurat')))")
+        cellhashR: \$(Rscript -e "library(cellhashR); cat(as.character(packageVersion('cellhashR')))")
     END_VERSIONS
     """
 }
--- modules/nf-core/bff/templates/bff.R
+++ modules/nf-core/bff/templates/bff.R
@@ -7,7 +7,8 @@
 ################################################
 
 # Helper function for NULL condition
-null_if <- function(x, val = "NULL") if (x == val) NULL else x
+string_to_null <- function(x, val = "NULL") if (x == val) NULL else x
+null_to_string <- function(x, val = "NULL") if (is.null(x)) val else x
 
 ################################################
 ################################################
@@ -20,20 +21,20 @@ null_if <- function(x, val = "NULL") if (x == val) NULL else x
 # input parameters
 hto_matrix <- '$hto_matrix'
 methods <- '$methods' # COMBINED, RAW, CLUSTER
-preprocessing <- '$preprocessing' #    preprocess_bff = task.ext.preprocess_bff ?: "FALSE"
+preprocessing <- '$preprocessing'
 
 # ext.args
-methodsForConsensus <- null_if('$methodsForConsensus')  # NULL, RAW, CLUSTER
-cellbarcodeWhitelist <- null_if('$cellbarcodeWhitelist')
-prefix <- 'prefix'
+methodsForConsensus <- string_to_null('$methodsForConsensus')  # NULL, RAW, CLUSTER
+cellbarcodeWhitelist <- string_to_null('$cellbarcodeWhitelist')
+prefix <- '$prefix'
 metricsFile <- paste0(prefix, "_metrics_bff.csv")
 doTSNE <- as.logical('$doTSNE')
-barcodeWhitelist <- null_if('$barcodeWhitelist')
+barcodeWhitelist <- string_to_null('$barcodeWhitelist')
 doHeatmap <- as.logical('$doHeatmap')
-perCellSaturation <- null_if('$perCellSaturation')
-majorityConsensusThreshold <- null_if('$majorityConsensusThreshold')
+perCellSaturation <- string_to_null('$perCellSaturation')
+majorityConsensusThreshold <- string_to_null('$majorityConsensusThreshold')
 chemistry <- '$chemistry'
-callerDisagreementThreshold <- null_if('$callerDisagreementThreshold')
+callerDisagreementThreshold <- string_to_null('$callerDisagreementThreshold')
 
 # check if the file exists
 if (! file.exists(hto_matrix)){
@@ -46,13 +47,8 @@ if (! file.exists(hto_matrix)){
 ################################################
 ################################################
 
-library(DropletUtils)
-library(Seurat)
-library(cellhashR)
-library(here)
-library(dplyr)
-library(ggplot2)
-library(argparse)
+library(Seurat) # for Read10X()
+library(cellhashR) # for ProcessCountMatrix() and GenerateCellHashingCalls()
 
 ################################################
 ################################################
@@ -60,12 +56,43 @@ library(argparse)
 ################################################
 ################################################
 
-# Loading Seurat object
-hashtag <- readRDS(seuratObj)
+# perform a basic preprocessing or direcly read the data
+if(preprocessing){
 
-# Demultiplex cells based on HTO enrichment
-hashtag <- HTODemux(hashtag, assay = assay, positive.quantile = quantile, init = init, nstarts = nstarts, kfunc = kfunc, seed = seed, verbose = verbose)
+  if(is.null(barcodeWhitelist)){
+    barcodes_list <- NULL
+  }else{
+    # separate the barcodes by comma and remove leading/trailing whitespace from each word
+    barcodes_list <- trimws(strsplit(barcodeWhitelist, ",")[[1]])
+  }
 
+  # perform preprocessing
+  counts <- ProcessCountMatrix(rawCountData = hto_matrix, barcodeWhitelist = barcodes_list)
+}else{
+    # perform preprocessing
+  counts <- Read10X(hto_matrix)
+}
+
+# determine the methods that should be used
+methods_input <- switch(
+  methods,
+  "COMBINED" = c("bff_raw", "bff_cluster"),
+  "RAW" = c("bff_raw"),
+  "CLUSTER" = c("bff_cluster"),
+  stop("'methods' must be either 'RAW', 'CLUSTER', or 'COMBINED'")
+)
+
+# determine the final consensus call based on RAW, CLUSTER or BOTH (NULL)
+methodsForConsensus_input = NULL
+if (!is.null(methodsForConsensus)) {
+    methodsForConsensus_input <- switch(methodsForConsensus,
+        "RAW" = "bff_raw",
+        "CLUSTER" = "bff_cluster",
+        stop("'methodsForConsensus' must be either 'RAW', 'CLUSTER', or NULL")
+    )
+}
+
+results <- GenerateCellHashingCalls(barcodeMatrix = counts, methods = methods_input, doTSNE = doTSNE, doHeatmap = doHeatmap, methodsForConsensus = methodsForConsensus_input,cellbarcodeWhitelist = cellbarcodeWhitelist,metricsFile = metricsFile, perCellSaturation = perCellSaturation, majorityConsensusThreshold = majorityConsensusThreshold, chemistry = chemistry, callerDisagreementThreshold = callerDisagreementThreshold)
 
 ################################################
 ################################################
@@ -73,24 +100,17 @@ hashtag <- HTODemux(hashtag, assay = assay, positive.quantile = quantile, init =
 ################################################
 ################################################
 
-# create a data frame to save the used parameters in a csv file
-if (is.null(init)) {
-  init <- "NULL"
-}
-
-Argument <- c("seuratObject", "quantile", "kfunc", "nstarts", "nsamples", "seed", "init", "assay", "verbose")
-Value <- c(seuratObj, quantile, kfunc, nstarts, nsamples, seed, init, assay, verbose)
+#saving parameters in a dataframe
+Argument <- c("hto_matrix", "methods", "methodsForConsensus", "cellbarcodeWhitelist", "metricsFile", "perCellSaturation","majorityConsensusThreshold","callerDisagreementThreshold", "doTSNE","doHeatmap","chemistry")
+Value <- c(hto_matrix, null_to_string(methods), null_to_string(methodsForConsensus), null_to_string(cellbarcodeWhitelist), metricsFile, null_to_string(perCellSaturation), null_to_string(majorityConsensusThreshold), null_to_string(callerDisagreementThreshold), doTSNE, doHeatmap, chemistry)
 params <- data.frame(Argument, Value)
+write.csv(params, paste0(prefix ,"_params_bff.csv"))
 
-write.csv(params, paste0(prefix ,"_params_htodemux.csv"))
-
-# create csv files to save the results from HTODemux()
-donors <- rownames(hashtag[[assay]])
-assignment <- hashtag[[paste0(assay, "_classification")]]
-assignment[[paste0(assay, "_classification")]][!assignment[[paste0(assay, "_classification")]] %in% c(donors, "Negative")] <- "Doublet"
-write.csv(assignment, paste0(prefix ,"_assignment_htodemux.csv"))
-write.csv(hashtag[[paste0(assay, "_classification.global")]], paste0(prefix ,"_classification_htodemux.csv"))
-saveRDS(hashtag, file = paste0(prefix ,"_htodemux.rds"))
+# save the results of GenerateCellHashingCalls or an empty dataframe if NULL
+if(is.null(results)){
+    results <- data.frame()
+}
+write.csv(results, paste0(prefix, "_assignment_bff.csv"), row.names=FALSE)
 
 ################################################
 ################################################
@@ -100,12 +120,14 @@ saveRDS(hashtag, file = paste0(prefix ,"_htodemux.rds"))
 
 r.version <- paste(R.version[['major']],R.version[['minor']], sep = ".")
 seurat.version <- as.character(packageVersion('Seurat'))
+cellhashR.version <- as.character(packageVersion('cellhashR'))
 
 writeLines(
     c(
         '"${task.process}":',
         paste('    r-base:', r.version),
-        paste('    seurat:', seurat.version)
+        paste('    r-seurat:', seurat.version),
+        paste('    cellhashR:', cellhashR.version)
     ),
 'versions.yml')
 
