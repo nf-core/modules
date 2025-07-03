3d0a306c19c0eb026f33ff9c06de77cee6bf9382
Maria Antonia Saracco <antonia.saracco@zs.com>
2025-07-03 01:59:54 +0000

    Update versioning command

--- modules/nf-core/decoupler/decoupler/templates/decoupler.py
+++ modules/nf-core/decoupler/decoupler/templates/decoupler.py
@@ -113,5 +113,4 @@ for result in results:
 
 ## VERSIONS FILE
 with open('versions.yml', 'a') as version_file:
-    version_file.write('"${task.process}":' + "\\n")
-    version_file.write("\tdecoupler-py: " + dc.__version__ + "\\n")
+    version_file.write("decoupler-py: " + dc.__version__ + "\\n")
