046e8a22bf90157b9f255eaa2cfefbb2e05e2cf8
Elena Buscaroli <ele.buscaroli@gmail.com>
2025-09-17 13:13:46 +0200

    adding argument and updating output names

--- modules/nf-core/mobster/templates/main_script.R
+++ modules/nf-core/mobster/templates/main_script.R
@@ -6,7 +6,6 @@ parse_args = function(x) {
     # giving errors when we have lists like c(xxx, xxx) since it will separate it
     # args_list = unlist(strsplit(x, ', ')[[1]])
     args_list = unlist(strsplit(x, ", (?=[^)]*(?:\\\\(|\$))", perl=TRUE))
-    # args_vals = lapply(args_list, function(x) strsplit(x, split=":")[[1]])
     args_vals = lapply(args_list, function(x) {
         x_splt = strsplit(x, split=":")[[1]]
         c(x_splt[1],  paste(x_splt[2:length(x_splt)], collapse=":"))
@@ -45,7 +44,8 @@ if ( grepl(".rds\$", tolower("$rds_join")) ) {
                            function(sample_name) {
                              purity = original[[sample_name]][["purity"]]
                              original[[sample_name]] %>%
-                               # keep only mutations on the diploid karyotypeCNAqc::subset_by_segment_karyotype("1:1") %>%
+                               # keep only mutations on the diploid karyotype
+                               CNAqc::subset_by_segment_karyotype("1:1") %>%
                                CNAqc::Mutations() %>%
                                dplyr::mutate(sample_id=sample_name, purity=purity)
                            }) %>% dplyr::bind_rows()
@@ -56,14 +56,17 @@ if ( grepl(".rds\$", tolower("$rds_join")) ) {
     input_table = read.csv("$rds_join")
 }
 
-## Function to run a single mobster fit
+## Function to run a single MOBSTER fit
 run_mobster_fit = function(joint_table, descr) {
     purity = unique(joint_table[["purity"]]) %>% as.numeric()
 
-    # get input table for the single patient
+    if(length(purity) > 1)
+        cli::cli_abort("Multiple values of purity are found in one sample (sample ID: {descr}).")
+
+    # remove mutations with VAF < min_VAF
     inp_tb = joint_table %>%
         dplyr::mutate(adj_VAF=VAF/!!purity) %>%
-        dplyr::filter(adj_VAF>=0.05) %>%
+        dplyr::filter(adj_VAF>=as.numeric(opt[["min_VAF"]])) %>%
         dplyr::filter(karyotype=="1:1") %>%
         dplyr::select(-adj_VAF)
 
@@ -75,10 +78,11 @@ run_mobster_fit = function(joint_table, descr) {
                 description = descr)
 }
 
+## Run MOBSTER on each sample
 lapply(samples, function(sample_name) {
 
     fit = run_mobster_fit(joint_table=input_table %>% dplyr::filter(sample_id == !!sample_name),
-                          descr=description)
+                          descr=paste(description, sample_name, sep=":"))
     
     if (any(fit[["fits.table"]][["tail"]])) {
         evoparams = evolutionary_parameters(fit)
@@ -89,19 +93,19 @@ lapply(samples, function(sample_name) {
     best_fit = fit[["best"]]
     plot_fit = plot(best_fit)
 
-    saveRDS(object=fit, file=paste0(opt[["prefix"]], "_mobster_st_fit.rds"))
-    saveRDS(object=best_fit, file=paste0(opt[["prefix"]], "_mobster_st_best_fit.rds"))
-    saveRDS(object=plot_fit, file=paste0(opt[["prefix"]], "_mobster_st_best_fit_plots.rds"))
+    saveRDS(object=fit, file=paste0(opt[["prefix"]], "_mobster_fit.rds"))
+    saveRDS(object=best_fit, file=paste0(opt[["prefix"]], "_mobster_best_fit.rds"))
+    saveRDS(object=plot_fit, file=paste0(opt[["prefix"]], "_mobster_best_plots.rds"))
 
     # save report plots
     report_fig = mobster::plot_model_selection(fit)
-    saveRDS(report_fig, file=paste0(opt[["prefix"]], "_REPORT_plots_mobster.rds"))
-    ggplot2::ggsave(plot=report_fig, filename=paste0(opt[["prefix"]], "_REPORT_plots_mobster.pdf"), height=210, width=210, units="mm", dpi = 200)
-    ggplot2::ggsave(plot=report_fig, filename=paste0(opt[["prefix"]], "_REPORT_plots_mobster.png"), height=210, width=210, units="mm", dpi = 200)
+    saveRDS(report_fig, file=paste0(opt[["prefix"]], "_mobster_report.rds"))
+    ggplot2::ggsave(plot=report_fig, filename=paste0(opt[["prefix"]], "_mobster_report.pdf"), height=210, width=210, units="mm", dpi = 200)
+    ggplot2::ggsave(plot=report_fig, filename=paste0(opt[["prefix"]], "_mobster_report.png"), height=210, width=210, units="mm", dpi = 200)
 })
 
 
-# version export
+# Version export #####
 f = file("versions.yml","w")
 cnaqc_version = sessionInfo()\$otherPkgs\$CNAqc\$Version
 mobster_version = sessionInfo()\$otherPkgs\$mobster\$Version
