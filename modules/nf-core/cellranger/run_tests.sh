629a883c22ba7b7ef38b17a7e82a5c0e1ad2956d
Alan Möbbs <64787947+alanmmobbs93@users.noreply.github.com>
2026-03-13 10:43:26 -0300

    Delete run_tests.sh

--- modules/nf-core/cellranger/run_tests.sh
+++
@@ -1,8 +0,0 @@
-cd /workspaces/modules
-
-#nf-test test modules/nf-core/cellranger/count  --profile docker --update-snapshot
-#nf-test test modules/nf-core/cellranger/mkgtf  --profile docker --update-snapshot
-#nf-test test modules/nf-core/cellranger/mkref  --profile docker --update-snapshot
-#nf-test test modules/nf-core/cellranger/mkvdjref  --profile docker --update-snapshot
-#nf-test test modules/nf-core/cellranger/multi  --profile docker --update-snapshot
-nf-test test modules/nf-core/cellranger/vdj  --profile docker --update-snapshot
