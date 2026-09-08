682115e3ac96fbd225515643bb5e244176f3c246
Donald Baku <141358602+dbaku42@users.noreply.github.com>
2026-09-08 10:00:21 +0200

    Update modules/nf-core/custom/pcaclustering/templates/clustering.py
    Co-authored-by: Niklas Schandry <schandry@gmail.com>

--- modules/nf-core/custom/pcaclustering/templates/clustering.py
+++ modules/nf-core/custom/pcaclustering/templates/clustering.py
@@ -38,7 +38,7 @@ ignore_cols = {"FID", "IID", "SID", "sample_id"}
         feature_cols = [col for col in df.columns if col not in plink_id_cols]
     elif "sample_id" in df.columns:
         sample_ids = df["sample_id"]
-        feature_cols = [col for col in df.columns if col != "sample_id"]
+      feature_cols = [col for col in df.columns if col not in ignore_cols]
     else:
         sample_ids = df.iloc[:, 0]
         feature_cols = list(df.columns[1:])
